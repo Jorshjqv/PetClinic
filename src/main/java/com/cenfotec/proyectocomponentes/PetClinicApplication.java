f9f2fcffb93abae2b3175cd3264187aeaf9b3aa0
Jorshjqv <Jorshaqv@gmail.com>
2020-04-30 18:35:21 -0600

    Added main entities, dependencies
    Main entities and their dependencies, changed main package name and controllers package

--- src/main/java/com/cenfotec/proyectocomponentes/PetClinicApplication.java
+++ src/main/java/com/cenfotec/proyectocomponentes/PetClinicApplication.java
@@ -1,4 +1,4 @@
-package com.ucenfotec.proyectocomponentes.PetClinic;
+package com.cenfotec.proyectocomponentes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
