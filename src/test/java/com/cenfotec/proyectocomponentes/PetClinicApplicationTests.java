b1625aef89b2e019620279bece678ead0481e2c8
Jorshjqv <Jorshaqv@gmail.com>
2020-05-01 22:09:47 -0600

    Update PetClinicApplicationTests.java

--- src/test/java/com/cenfotec/proyectocomponentes/PetClinicApplicationTests.java
+++ src/test/java/com/cenfotec/proyectocomponentes/PetClinicApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ucenfotec.proyectocomponentes.PetClinic;
+package com.cenfotec.proyectocomponentes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
